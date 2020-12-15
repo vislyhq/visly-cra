6413f37fdf4e2f26c6634b9094afe8e18f9e9bca
Emil Sjölander <emil@visly.app>
2020-12-15 13:41:40 +0000

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders text on next hint button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const button = screen.getByText(/Next Hint/gmi);
+  expect(button).toBeDefined();
 });
